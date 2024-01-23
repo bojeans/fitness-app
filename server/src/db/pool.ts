5645df6f2901b0150631b3da4f350f0f699e049f
Chris Anderson <chris.d.anderson87@gmail.com>
2024-01-23 14:35:44 +1300

    hardcoded values working on /users on backend

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -1,12 +1,34 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const port = process.env.POSTGRES_PORT || "5432";
 
-const pool = new Pool({
+console.log("PostgreSQL connection parameters:", {
+  host: process.env.POSTGRES_HOST,
   user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
+  port: parseInt(port, 10),
   password: process.env.POSTGRES_PASSWORD,
-  port: parseInt(port),
+  database: process.env.POSTGRES_DB,
 });
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST || "fitness-app-db-1",
+  user: process.env.POSTGRES_USER || "postgres" || "chrisanderson",
+  port: parseInt(port, 10),
+  password: process.env.POSTGRES_PASSWORD || "password",
+  database: process.env.POSTGRES_DB || "postgres",
+});
+
+console.log(pool);
+
+// const pool = new Pool({
+//   host: process.env.POSTGRES_HOST || "fitness-app-db-1",
+//   user: process.env.POSTGRES_USER,
+//   port: parseInt(port, 10),
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DB,
+// });
 
 export default pool;
