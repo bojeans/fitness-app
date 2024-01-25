b5d140fa07218d9988d27f17f57a656a399cbe9c
Chris Anderson <chris.d.anderson87@gmail.com>
2024-01-25 22:01:41 +1300

    combined env files as one wasn't being read

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -4,20 +4,20 @@ import { Pool, QueryResult } from "pg";
 
 const port = process.env.POSTGRES_PORT || "5432";
 
-console.log("PostgreSQL connection parameters:", {
+// console.log("PostgreSQL connection parameters:", {
+//   host: process.env.POSTGRES_HOST,
+//   user: process.env.POSTGRES_USER,
+//   port: parseInt(port, 10),
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DB,
+// });
+
+const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   user: process.env.POSTGRES_USER,
   port: parseInt(port, 10),
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-});
-
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST || "localhost",
-  user: process.env.POSTGRES_USER || "postgres",
-  port: parseInt(port, 10),
-  password: process.env.POSTGRES_PASSWORD || "password",
-  database: process.env.POSTGRES_DB || "postgres",
 });
 
 const connectToDatabase = async () => {
