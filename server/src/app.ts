2ee90ad2261678b6be9a9d009b773139c9e7685c
Chris Anderson <chris.d.anderson87@gmail.com>
2024-02-05 11:32:03 +1300

    adding custom docker start script & remove openapi from app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,21 +2,12 @@ import express from "express";
 import cors from "cors";
 import usersRouter from "./routes/users";
 import testRouter from "./routes/simpleRouteTest";
-import swaggerUI from "swagger-ui-express";
-import fs from "fs";
-import path from "path";
-import yaml from "js-yaml";
-import { JsonObject } from "swagger-ui-express";
 
 const app = express();
-const swaggerFile: JsonObject = yaml.load(
-  fs.readFileSync(path.join(__dirname, "../../"), "utf8")
-) as JsonObject;
 
 // Middlewares
 app.use(cors());
 app.use("/users", usersRouter);
 app.use("/test", testRouter);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 export default app;
