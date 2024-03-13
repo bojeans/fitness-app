ba168dce4e0663ccd73a176dd6764b012be14fc4
Chris Anderson <chris.d.anderson87@gmail.com>
2024-03-13 15:38:35 +1300

    jest config from nextjs docs

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -3,8 +3,12 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
 
+const createJestConfig = nextJest({
+  dir: "./",
+});
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -196,4 +200,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
