6343a232f718b025c81fba09c8209d7b98584018
Chris Anderson <chris.d.anderson87@gmail.com>
2024-03-14 16:10:52 +1300

    adding setup jest file and node env

--- client/src/__tests__/api.test.tsx
+++ client/src/__tests__/api.test.tsx
@@ -9,19 +9,38 @@ describe("Fetch from API", () => {
     (global.fetch as jest.Mock).mockClear();
   });
 
-  it("calls http with the right URL for getUsers", async () => {
+  it("calls http with the right URL for getUsers in development environment", async () => {
     // Mock fetch to resolve with an empty array
     (global.fetch as jest.Mock).mockResolvedValue({
       ok: true,
       json: () => Promise.resolve([]),
     });
 
+    // Set NODE_ENV to "development" before calling getUsers
+    process.env.NODE_ENV = "development";
+
+    await api.getUsers();
+
+    expect(global.fetch).toHaveBeenCalledWith("http://localhost:5001/users");
+  });
+
+  it("calls http with the right URL for getUsers in production environment", async () => {
+    // Mock fetch to resolve with an empty array
+    (global.fetch as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve([]),
+    });
+
+    // Set NODE_ENV to "production" before calling getUsers
+    process.env.NODE_ENV = "production";
+
     await api.getUsers();
 
     expect(global.fetch).toHaveBeenCalledWith(
-      `${process.env.NEXT_PUBLIC_API_URL}/users`
+      "https://fitness-app-beryl.vercel.app/users"
     );
   });
+
   it("throws an error if the response is not ok", async () => {
     // Mock fetch to resolve with an error
     (global.fetch as jest.Mock).mockResolvedValue({
@@ -29,28 +48,9 @@ describe("Fetch from API", () => {
       status: 404,
     });
 
+    // Set NODE_ENV to "development" before calling getUsers
+    process.env.NODE_ENV = "development";
+
     await expect(api.getUsers()).rejects.toThrow("HTTP error! status: 404");
-  });
-});
-
-describe("Error Handling for Production Environment Variable", () => {
-  beforeAll(() => {
-    // Unset NEXT_PUBLIC_API_URL or set it to an unexpected value
-    delete process.env.NEXT_PUBLIC_API_URL;
-    // OR
-    process.env.NEXT_PUBLIC_API_URL = "unexpected-value";
-  });
-
-  afterAll(() => {
-    // Reset environment variable after tests
-    process.env.NEXT_PUBLIC_API_URL = "https://fitness-app-beryl.vercel.app";
-  });
-
-  it("throws an error when NEXT_PUBLIC_API_URL is undefined", async () => {
-    await expect(api.getUsers()).rejects.toThrow("API URL is not defined");
-  });
-
-  it("throws an error when NEXT_PUBLIC_API_URL is set to an unexpected value", async () => {
-    await expect(api.getUsers()).rejects.toThrow("Invalid API URL");
   });
 });
--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,25 +1,18 @@
-require("dotenv").config();
-
-console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
-
 const http = async (url: string) => {
   const response = await fetch(url);
   if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}, URL: ${url}`);
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
   return await response.json();
 };
 
 const api = {
-  getUsers: () => {
-    if (
-      !process.env.NEXT_PUBLIC_API_URL ||
-      process.env.NEXT_PUBLIC_API_URL === "unexpected-value"
-    ) {
-      throw new Error("API URL is not defined or is invalid");
-    }
-    return http(`${process.env.NEXT_PUBLIC_API_URL}/users`);
-  },
+  getUsers: () =>
+    http(
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:5001/users"
+        : "https://fitness-app-beryl.vercel.app/users"
+    ),
 };
 
 export default api;
